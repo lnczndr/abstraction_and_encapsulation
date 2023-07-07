93722bc647832934f429c9b1870eb8838d2d4e5e
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-08 01:01:36 +0800

    pseudocode

---
+++ car_class/test_car.py
@@ -0,0 +1,14 @@
+# LEGASPI, LANA CAZANDRA U.
+# BSCPE 1-5
+
+# ASSIGNMENT 9: ABSTRACTION AND ENCAPSULATION
+
+# PSEUDOCODE
+
+# import Car from car_class
+
+# create a test car object
+
+# accelerate five times, while displaying speed
+
+# brake five times, while displaying speed
