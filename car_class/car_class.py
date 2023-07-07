74a0029f807dee9bed32f57f5a7335086076ecfb
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-08 00:57:09 +0800

    pseudocode

---
+++ car_class/car_class.py
@@ -0,0 +1,17 @@
+# LEGASPI, LANA CAZANDRA U.
+# BSCPE 1-5
+
+# ASSIGNMENT 9: ABSTRACTION AND ENCAPSULATION
+
+# PSEUDOCODE
+
+# making car class
+
+# using constructor to assign values
+
+# defining accelerate method
+
+# defining brake method
+
+# defining get_speed method
+
