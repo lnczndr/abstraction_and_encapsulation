1cb4d1a18b13b6b8b1ce886342de0e7a2e9ce81e
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-08 01:40:52 +0800

    making of constructor

--- pet_class/pet_class.py
+++ pet_class/pet_class.py
@@ -6,16 +6,21 @@
 # PSEUDOCODE
 
 # making pet class
+class Pet:
 
-# using constructor to assign values
+    # using constructor to assign values
+    def __init__(self):
+        self.__name=""
+        self.__animal_kind=""
+        self.__age= 0
 
 # specifying name of the pet
 # set name of the pet
 # get name of the pet
 
-# specifying the type of the pet
-# set type of the pet
-# get type of the pet
+# specifying the kind of  pet
+# set kind of pet
+# get kimd of pet
 
 # specifying the age of the pet
 # set age of the pet
