5837e6399a9f4713e07dd9990cbf63356f175d01
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-08 00:24:37 +0800

    output for fan 1 and 2

--- test_fan.py
+++ test_fan.py
@@ -16,7 +16,17 @@ def main():
     # assign values for fan 2
     fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
-# output:
-# fan 1
+    # output:
+    # fan 1
+    print("Fan 1:")
+    print("Fan Speed:", fan1.speed_get())
+    print("Fan Radius:", fan1.radius_get())
+    print("Fan Color:", fan1.color_get())
+    print("On:", fan1.is_on())
 
-# fan 2
+    # fan 2
+    print("\nFan 2:")
+    print("Fan Speed:", fan2.speed_get())
+    print("Fan Radius:", fan2.radius_get())
+    print("Fan Color:", fan2.color_get())
+    print("On:", fan2.is_on())
