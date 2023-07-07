cafa37bd3ba37eccd2d037d1a0c05f7d091fca08
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-08 01:35:27 +0800

    pseudocode

---
+++ pet_class/pet_class.py
@@ -0,0 +1,22 @@
+# LEGASPI, LANA CAZANDRA U.
+# BSCPE 1-5
+
+# ASSIGNMENT 9: ABSTRACTION AND ENCAPSULATION
+
+# PSEUDOCODE
+
+# making pet class
+
+# using constructor to assign values
+
+# specifying name of the pet
+# set name of the pet
+# get name of the pet
+
+# specifying the type of the pet
+# set type of the pet
+# get type of the pet
+
+# specifying the age of the pet
+# set age of the pet
+# get age of the pet
---
+++ pet_class/test_pet.py
@@ -0,0 +1,14 @@
+# LEGASPI, LANA CAZANDRA U.
+# BSCPE 1-5
+
+# ASSIGNMENT 9: ABSTRACTION AND ENCAPSULATION
+
+# PSEUDOCODE
+
+# import pet from pet_class
+
+# create a pet object 
+
+# ask user for input regarding the details of the pet
+
+# displays the pet details via output
