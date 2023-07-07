8b8561c5cd67145e607034e9e578d29a1eb2cf27
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-08 02:05:29 +0800

    input for kind of animal

--- pet_class/test_pet.py
+++ pet_class/test_pet.py
@@ -15,5 +15,10 @@ def main():
     name = input(str("What is your pet's name? : "))
     user_pet.name_set(name)
 
+    animal_kind = input(str("What kind of pet do you have? : "))
+    user_pet.kind_set(animal_kind)
+
+    
+
 
 # displays the pet details via output
