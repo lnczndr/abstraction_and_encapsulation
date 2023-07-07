03e24d20e34274766fdc9c7366bd511e72ca2d05
Lana Cazandra Legaspi <lanacazandra@gmail.com>
2023-07-07 22:36:35 +0800

    making pseudocode for fan_class and test_fan

---
+++ fan_class.py
@@ -0,0 +1,19 @@
+# LEGASPI, LANA CAZANDRA U.
+# BSCPE 1-5
+
+# ASSIGNMENT 9: ABSTRACTION AND ENCAPSULATION
+
+# PSEUDOCODE
+
+# making fan class
+
+# assigning values to 3 constants (slow, medium, fast)
+
+# specifying the radius of the fan
+
+# specifying the color of the fan
+
+# using constructor to assign values
+
+# implementing getter and setter for four data fields
+
---
+++ test_fan.py
@@ -0,0 +1,19 @@
+# LEGASPI, LANA CAZANDRA U.
+# BSCPE 1-5
+
+# ASSIGNMENT 9: ABSTRACTION AND ENCAPSULATION
+
+# PSEUDOCODE
+
+# making test_fan class
+
+# defining main method
+
+# assign values for fan 1
+
+# assign values for fan 2
+
+# output:
+# fan 1
+
+# fan 2
